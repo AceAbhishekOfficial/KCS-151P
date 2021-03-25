89ca893c6015a7d755644d26eaf7f20d091a16b9
Abhishek Srivastava <72290060+AceAbhishekOfficial@users.noreply.github.com>
2021-03-25 09:26:05 +0530

    largest of 3
    Write a program to find the largest of three numbers using nested if else.

---
+++ p4.c
@@ -0,0 +1,33 @@
+/*
+                WAP to find largest of three numbers using nested if else
+                        Code By Abhishek
+*/
+#include <stdio.h>
+int main()
+{
+    int a , b, c;
+    printf("Enter three numbers\n");
+    scanf("%i %i %i",&a,&b,&c);
+    if(a>b)
+    {
+        if(a>c)
+        printf("Largest number is %i",a);
+        else
+        printf("Largest number is %i",c);
+    }
+    if(a>c)
+    {
+        if(a>b)
+        printf("Largest number is %i",a);
+        else
+        printf("Largest number is %i",b);
+    }
+    else
+    {
+        if(b>c)
+        printf("Largest number is %i",b);
+        else
+        printf("Largest number is %i",c);
+    }
+    return 1;
+}
