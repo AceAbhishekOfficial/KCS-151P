c778a5217e55f63367c29a75872b56a736f33d2a
Abhishek Srivastava <72290060+AceAbhishekOfficial@users.noreply.github.com>
2021-03-28 19:34:36 +0530

    Delete duplicate elements in array
    Write a program to delete duplicate element in a list of 10 elements &
    display it on screen.

--- p25.c
+++ p25.c
@@ -1,19 +1,24 @@
+/*
+                Write a program to delete duplicate element in a list of 10 elements &
+                display it on screen. 
+                            Code by Abhishek
+*/
 #include <stdio.h>
 int main()
 {
-    int arr[10];
-    int arr2[10];
+    int arr[10]; //orginal array
+    int arr2[10]; //new array consists of unique elements
     int c=1;
     printf("Enter 10 numbers one by one\n");
-    for (int i = 0; i < sizeof(arr)/4; i++)
+    for (int i = 0; i < sizeof(arr)/4; i++)     // looop to input array
     {
         scanf("%i",&arr[i]);
     }
     arr2[0]=arr[0];
-    for(int a = 1; a<sizeof(arr)/4;a++)
+    for(int a = 1; a<sizeof(arr)/4;a++)     //loop to copy array
     {      
         int flag =0;
-        for(int b = 0; b<a;b++)
+        for(int b = 0; b<a;b++)         // this loop makes sures element is not repeated
         if(arr[a]==arr[b])
         flag=1;
 
@@ -23,8 +28,7 @@ int main()
             c++;
         }
     }
-    printf("Elemts in array are: %i\n",c);
-    
+    printf("Elemts in array are: %i\n",c);   
     for(int a =0;a<c;a++)
     printf("%i,",arr2[a]);
     
