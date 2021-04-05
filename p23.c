a869b8ddc2ca77932a998a9d74c033b03ef63aaf
Abhishek Srivastava <72290060+AceAbhishekOfficial@users.noreply.github.com>
2021-04-05 09:33:51 +0530

    Decimal to Binary
    Write a program to convert decimal number in to binary number.

--- p23.c
+++ p23.c
@@ -8,29 +8,19 @@
 void reverseStr(char str[]);
 int main()
 {
-    char bin[32];
+    char bin[32],temp[32];
     int num;
     printf("Enter number\n");
     scanf("%i",&num);
     int c=0;
     while(num!=0)
     {
-        bin[c++]= (char)((num%2)+48);   //remainder converting in to char by adding 48 (refer to ASCII)
+        sprintf(temp,"%i%s",num%2,bin);  // temp = (n%2) + bin
+        sprintf(bin,"%s",temp); //bin = temp
+        c++;
         num/=2;
     }
-    bin[c]='\0';    // marking end of string
-    reverseStr(bin);  //reversing the string
+    bin[c]='\0';// marking end of string
     printf("%s",bin);
     return 1;
-}
-void reverseStr(char str[])     // reverse a string function
-{
-  int n = strlen(str);
-
-  for (int i = 0; i < n / 2; i++)
-  {
-    char ch = str[i];
-    str[i] = str[n - i - 1];
-    str[n - i - 1] = ch;
-  }
 }
