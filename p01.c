37fa10a06c41a07f2a3ccb6d19d2a5532799cacb
Abhishek Srivastava <72290060+AceAbhishekOfficial@users.noreply.github.com>
2021-04-27 18:27:07 +0530

    Updated p1.c

--- p01.c
+++ p01.c
@@ -9,7 +9,7 @@ int main()
 {
     float s1 = 4, s2 = 5, s3 =6;
     float s=(s1+s2+s3)/2;
-    float area = (s*(s-s1)*(s-s2)*(s-s3));
+    float area = sqrt(s*(s-s1)*(s-s2)*(s-s3));
     if(area<=0)
     printf("Traingle not possible");
     printf("Area is %f",area);
